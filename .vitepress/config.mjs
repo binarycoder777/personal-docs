cd11bbb7745491be7551bad91bdc0f0fc4bad6bf
atao <87337334@qq.com>
2024-02-20 17:58:18 +0800

    Apache Lucene 简介

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -29,30 +29,12 @@ export default defineConfig({
 
     sidebar: {
       '/docs/article/': [
-          {
-            text: '2024',
-            collapsed: true,
-            items: [
-              { 
-                text: '备考信息系统项目管理师',
-                collapsed: true,
-                items: [
-                  {
-                    text: 'Apache Lucene简介',
-                    link: ''
-                  }
-                ]
-              }
-            ]
-          },
-      ],
-      '/docs/book/': [
         {
-          text: 'ElasticSearch系列',
+          text: '2024',
           collapsed: true,
           items: [
-            { 
-              text: '深入理解ElasticSearch',
+            {
+              text: '2月',
               collapsed: true,
               items: [
                 {
@@ -60,34 +42,52 @@ export default defineConfig({
                   link: ''
                 }
               ]
-            },
-            { 
-              text: 'ElasticSearch源码解析与优化实战', 
-              collapsed: true,
-              items:[
-                {
-                  text: '走进ElasticSearch',
-                  link: ''
-                }
-              ]
             }
           ]
-        },
-      ],
-      '/docs/project/': [
-        {
-          text: '基于Astro的个人站点搭建',
-          items: [
-            { text: 'Markdown Examples', link: '/markdown-examples' },
-            { text: 'Runtime API Examples', link: '/api-examples' }
-          ]
-        },
+        }
       ]
     },
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    '/docs/book/': [
+      {
+        text: 'ElasticSearch系列',
+        collapsed: true,
+        items: [
+          {
+            text: '深入理解ElasticSearch',
+            collapsed: true,
+            items: [
+              {
+                text: 'Apache Lucene简介',
+                link: ''
+              }
+            ]
+          },
+          {
+            text: 'ElasticSearch源码解析与优化实战',
+            collapsed: true,
+            items: [
+              {
+                text: '走进ElasticSearch',
+                link: ''
+              }
+            ]
+          }
+        ]
+      },
+    ],
+    '/docs/project/': [
+      {
+        text: '基于Astro的个人站点搭建',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      },
     ]
-  }
+  },
+
+  socialLinks: [
+    { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+  ]
 })
 
