61cf68a597af7fdd1d48a22a417afe0bc08481b5
atao <87337334@qq.com>
2024-07-04 17:37:38 +0800

    update style

--- .vitepress/theme/components/PersonalInfo.vue
+++ .vitepress/theme/components/PersonalInfo.vue
@@ -1,116 +1,207 @@
 <template>
+  <div class="scroll-background">
+    <div class="scroll">
+      <video autoplay muted loop playsinline class="painting-video">
+        <source src="../../public/intro.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div class="video-button">
+        <a class="btn" href="#aboutSection">探索</a>
+      </div>
+    </div>
+  </div>
   <div class="container">
     <div class="introduction">
-      <h1>关于我</h1>
+      <div class="language-stats">
+        <img
+          id="profile-img"
+          src="../../public/favicon.ico"
+          alt="BinaryCoder777"
+          @mouseenter="startRotation"
+          @mouseleave="stopRotation"
+          :style="{ transform: rotation }"
+        />
+      </div>
+      <h1 id="aboutSection" style="text-align: center">关于我</h1>
       <br />
-
       <p>
-        👋 一名软件开发工程师，专业领域涵盖了后端开发、数据库设计和系统架构，专注于将复杂的问题转化为简单而直观的设计。
+        👋
+        一名软件开发工程师，专业领域涵盖了后端开发、数据库设计和系统架构，专注于将复杂的问题转化为简单而直观的设计。
       </p>
       <br />
       <p>
-        💻 坚信技术的价值在于为用户创造价值，努力构建出简洁、高效且易于维护的代码，以提供出色的用户体验。
+        💻
+        坚信技术的价值在于为用户创造价值，努力构建出简洁、高效且易于维护的代码，以提供出色的用户体验。
       </p>
       <br />
       <p>
-        🚀 注重团队协作，喜欢和团队成员分享经验、学习新技术，并持续关注行业的最新动态，以确保我的技术栈始终保持更新。
+        🚀
+        注重团队协作，喜欢和团队成员分享经验、学习新技术，并持续关注行业的最新动态，以确保我的技术栈始终保持更新。
       </p>
       <br />
       <p>
-        🌐 当不在编写代码的时候，热衷于探索互联网世界。可能在学习新的编程语言，研究新的技术趋势，或者在个人项目上挥洒创意。
+        🌐
+        当不在编写代码的时候，热衷于探索互联网世界。可能在学习新的编程语言，研究新的技术趋势，或者在个人项目上挥洒创意。
       </p>
       <br />
       <div class="social-links">
-        <a href="https://github.com/binarycoder777" target="_blank" class="tooltip" data-title="GitHub">
-          <font-awesome-icon :icon="['fab', 'github']" /> 
+        <a
+          href="https://github.com/binarycoder777"
+          target="_blank"
+          class="tooltip"
+          data-title="GitHub"
+        >
+          <font-awesome-icon :icon="['fab', 'github']" />
         </a>
         <a href="mailto:atao67276@gmail.com" class="tooltip" data-title="Email">
-          <font-awesome-icon :icon="['fas', 'envelope']" /> 
+          <font-awesome-icon :icon="['fas', 'envelope']" />
         </a>
-        <a href="https://raw.githubusercontent.com/binarycoder777/personal-pic/main/pic/vx.jpg" target="_blank" class="tooltip" data-title="微信">
-          <font-awesome-icon :icon="['fab', 'weixin']" /> 
+        <a
+          href="https://raw.githubusercontent.com/binarycoder777/personal-pic/main/pic/vx.jpg"
+          target="_blank"
+          class="tooltip"
+          data-title="微信"
+        >
+          <font-awesome-icon :icon="['fab', 'weixin']" />
         </a>
       </div>
     </div>
-    <div class="language-stats">
-      <!-- <h2>Most Used Languages</h2> -->
-      <img src='https://github-readme-stats.vercel.app/api/top-langs/?username=BinaryCoder777&layout=donut&langs_count=10'
-        alt="Most Used Languages" />
+    <div class="features-section">
+      <div class="language-stats">
+        <h2>Most Used Languages</h2>
+        <img
+          src="https://github-readme-stats.vercel.app/api/top-langs/?username=BinaryCoder777&layout=donut&langs_count=10"
+          alt="Most Used Languages"
+        />
+      </div>
+      <h2 class="features-title">当前正在做的...</h2>
+      <p class="features-subtitle">
+        探索创新的技术解决方案，让编程之旅更加有趣。
+      </p>
+      <div class="features-cards">
+        <div class="feature-card">
+          <div class="card-icon">
+            <!-- 假设您使用的是图像文件 -->
+            <!-- <img src="/path-to-your-icon-1.png" alt="文档解析"> -->
+          </div>
+          <h3 class="card-title">系统解析方案</h3>
+          <p class="card-detail">
+            为客户提供定制化的专业解决方案，以满足特定行业的独特需求。
+          </p>
+        </div>
+        <div class="feature-card">
+          <div class="card-icon">
+            <!-- <img src="/path-to-your-icon-2.png" alt="软件开发"> -->
+          </div>
+          <h3 class="card-title">软件开发</h3>
+          <p class="card-detail">
+            承诺提供卓越的软件开发服务，确保每一次交付都是高效和高质量的。
+          </p>
+        </div>
+        <div class="feature-card">
+          <div class="card-icon">
+            <!-- <img src="/path-to-your-icon-3.png" alt="数据分析"> -->
+          </div>
+          <h3 class="card-title">数字创新</h3>
+          <p class="card-detail">
+            挖掘生活中的创新灵感，将酷炫的想法转化为实际可行的数字产品。
+          </p>
+        </div>
+      </div>
     </div>
   </div>
-  <div class="features-section">
-        <h2 class="features-title">当前正在做的...</h2>
-        <p class="features-subtitle">探索创新的技术解决方案，让编程之旅更加有趣。</p>
-        <div class="features-cards">
-            <div class="feature-card">
-                <div class="card-icon">
-                    <!-- 假设您使用的是图像文件 -->
-                    <!-- <img src="/path-to-your-icon-1.png" alt="文档解析"> -->
-                </div>
-                <h3 class="card-title">系统解析方案</h3>
-                <p class="card-detail">为客户提供定制化的专业解决方案，以满足特定行业的独特需求。</p>
-            </div>
-            <div class="feature-card">
-                <div class="card-icon">
-                    <!-- <img src="/path-to-your-icon-2.png" alt="软件开发"> -->
-                </div>
-                <h3 class="card-title">软件开发</h3>
-                <p class="card-detail">承诺提供卓越的软件开发服务，确保每一次交付都是高效和高质量的。</p>
-            </div>
-            <div class="feature-card">
-                <div class="card-icon">
-                    <!-- <img src="/path-to-your-icon-3.png" alt="数据分析"> -->
-                </div>
-                <h3 class="card-title">数字创新</h3>
-                <p class="card-detail">挖掘生活中的创新灵感，将酷炫的想法转化为实际可行的数字产品。</p>
-            </div>
-        </div>
-    </div>
-    <div class="lastes-news">
-        <h1>近期动态</h1>
-        <a href="/markdown-examples">阅览更多...</a>
-        <div class="features-cards">
-            <div class="feature-card">
-                <div class="card-icon">
-                    <!-- 假设您使用的是图像文件 -->
-                    <!-- <img src="/path-to-your-icon-1.png" alt="文档解析"> -->
-                </div>
-                <h3 class="card-title">系统解析方案</h3>
-                <p class="card-detail">为客户提供定制化的专业解决方案，以满足特定行业的独特需求。</p>
-            </div>
-            <div class="feature-card">
-                <div class="card-icon">
-                    <!-- <img src="/path-to-your-icon-2.png" alt="软件开发"> -->
-                </div>
-                <h3 class="card-title">软件开发</h3>
-                <p class="card-detail">承诺提供卓越的软件开发服务，确保每一次交付都是高效和高质量的。</p>
-            </div>
-            <div class="feature-card">
-                <div class="card-icon">
-                    <!-- <img src="/path-to-your-icon-3.png" alt="数据分析"> -->
-                </div>
-                <h3 class="card-title">数字创新</h3>
-                <p class="card-detail">挖掘生活中的创新灵感，将酷炫的想法转化为实际可行的数字产品。</p>
-            </div>
-        </div>
-    </div>
 </template>
 
 <script>
+
 export default {
-  name: 'PersonalInfoWithStats'
-}
+  name: "PersonalInfoWithStats",
+  data() {
+    return {
+      rotation: "rotate(0deg)", // 初始角度为0度
+    };
+  },
+  methods: {
+    startRotation() {
+      this.rotation = "rotate(360deg)"; // 鼠标移入时，设置角度为360度
+    },
+    stopRotation() {
+      this.rotation = "rotate(0deg)"; // 鼠标移出时，恢复初始角度为0度
+    }
+  },
+};
 </script>
 
 
 <style scoped>
+
+#profile-img {
+  width: 60%;
+  height: 60%;
+  transition: transform 0.5s ease; /* 添加过渡效果 */
+}
+
+.scroll-background {
+  background-image: url("../../public/juanzhou.png"); /* 卷轴背景图片路径 */
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center bottom;
+  padding: 4% 12%; /* 上方留出20%的空间，左右各留出10%的空间 */
+  position: relative;
+  overflow: hidden; /* 隐藏超出部分 */
+  z-index: -1; /* 设置较低的层叠顺序 */
+}
+
+.scroll {
+  position: relative;
+  height: 100%;
+  width: 85%; /* 卷轴宽度 */
+  margin: 0% auto; /* 居中卷轴 */
+  margin-bottom: 18%;
+  z-index: 1; /* 设置较高的层叠顺序，确保卷轴在上方内容之上 */
+}
+
+.painting-video {
+  width: 100%; /* 视频宽度与卷轴宽度一致 */
+  height: 100%; /* 根据宽度自动调整高度 */
+  display: block;
+  object-fit: cover; /* 确保视频充满卷轴 */
+  z-index: 1; /* 设置较低的层叠顺序，确保视频位于卷轴下方 */
+}
+
+.video-button {
+  position: absolute;
+  bottom: 20%; /* 距离底部的距离 */
+  left: 50%;
+  transform: translateX(-50%);
+  text-align: center;
+  width: 100%; /* 让按钮宽度充满父容器 */
+}
+
+.btn {
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #007bff;
+  color: white;
+  text-decoration: none;
+  border-radius: 5px;
+  font-size: 18px;
+  transition: transform 0.3s ease; /* 添加 transform 的过渡效果 */
+}
+
+.btn:hover {
+  transform: scale(5); /* 悬停时放大按钮 */
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3); /* 添加阴影效果 */
+}
+
 .container {
+  position: relative;
   display: flex;
-  max-width: 60%;
+  max-width: 80%;
   margin: auto;
   box-shadow: 0 2px 15px rgba(0, 0, 0, 0.1);
   margin-bottom: 50px;
-  margin-top: 50px;
+  margin-top: -10%;
 }
 
 .introduction {
@@ -183,7 +274,7 @@ export default {
   padding: 8px 12px; /* 内边距 */
   border-radius: 6px; /* 圆角 */
   font-size: 14px; /* 文字大小 */
-  box-shadow: 0 2px 4px rgba(0,0,0,0.2); /* 阴影效果 */
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2); /* 阴影效果 */
   z-index: 1000;
 }
 
@@ -203,8 +294,6 @@ export default {
   visibility: visible;
   opacity: 1;
 }
-
-
 
 .language-stats {
   flex: 1;
@@ -226,75 +315,76 @@ export default {
   height: auto;
 }
 
-
 .lastes-news {
-    padding: 2rem;
-    box-shadow: 0 2px 15px rgba(0, 0, 0, 0.1);
-    text-align: center;
-    max-width: 60%;
-    margin-left: auto;
-    margin-right: auto;
-    margin-bottom: 20px;
+  padding: 2rem;
+  box-shadow: 0 2px 15px rgba(0, 0, 0, 0.1);
+  text-align: center;
+  max-width: 60%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 20px;
 }
 
 .lastes-news h1 {
-    font-size: 1.5rem;
-    margin-bottom: 0.5rem;
+  font-size: 1.5rem;
+  margin-bottom: 0.5rem;
 }
 
 .features-section {
-    text-align: center;
-    padding: 2rem;
-    /* background-color: #f9f9f9; 根据实际需求调整 */
-    box-shadow: 0 2px 15px rgba(0, 0, 0, 0.1);
-    max-width: 60%;
-    margin-bottom: 50px;
-    margin-left: auto;
-    margin-right: auto;
+  text-align: center;
+  padding: 2rem;
+  /* background-color: #f9f9f9; 根据实际需求调整 */
+  /* box-shadow: 0 2px 15px rgba(0, 0, 0, 0.1); */
+  max-width: 60%;
+  margin-bottom: 50px;
+  margin-left: auto;
+  margin-right: auto;
 }
 
 .features-title {
-    font-size: 1.5rem;
-    margin-bottom: 0.5rem;
+  margin-top: 10%;
+  font-size: 1.5rem;
+  margin-bottom: 0.5rem;
 }
 
 .features-subtitle {
-    margin-bottom: 2rem;
-    color: #666;
+  margin-bottom: 2rem;
+  color: #666;
 }
 
 .features-cards {
-    display: flex;
-    justify-content: space-around;
+  display: flex;
+  justify-content: space-around;
 }
 
 .feature-card {
-    width: 300px;
-    /* 根据实际需求调整 */
-    padding: 1rem;
-    /* background: #ffffff; */
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    border-radius: 8px;
-    margin: 0 1rem;
+  width: 300px;
+  /* 根据实际需求调整 */
+  padding: 1rem;
+  /* background: #ffffff; */
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 8px;
+  margin: 0 1rem;
 }
 
 .card-icon {
-    margin-bottom: 1rem;
+  margin-bottom: 1rem;
 }
 
 .card-icon img {
-    width: 50px;
-    /* 根据实际需求调整 */
+  width: 50px;
+  /* 根据实际需求调整 */
 }
 
 .card-title {
-    font-size: 1.2rem;
-    /* color: #333; */
-    margin-bottom: 0.5rem;
+  font-size: 1.2rem;
+  /* color: #333; */
+  margin-bottom: 0.5rem;
 }
 
 .card-detail {
-    /* color: #666; */
-    font-size: 0.9rem;
+  /* color: #666; */
+  font-size: 0.9rem;
 }
-/* Add more styles as necessary */</style>
+/* Add more styles as necessary */
+</style>
--- index.md
+++ index.md
@@ -1,17 +1,17 @@
 ---
 layout: home
-hero:
-  name: BinaryCoder777
-  text: 欢迎您的访问
-  tagline: 期待着与您共同探讨技术、创新和软件开发的精彩之处
-  image: https://binarycoder777-site.oss-cn-chengdu.aliyuncs.com/21707272915_.pic.jpg
-  actions:
-    - theme: brand
-      text: 开始探索
-      link: /docs/article/markdown-examples
-    - theme: alt
-      text: 项目
-      link: /docs/article/markdown-examples
+# hero:
+#   name: BinaryCoder777
+#   text: 欢迎您的访问
+#   tagline: 期待着与您共同探讨技术、创新和软件开发的精彩之处
+#   image: https://binarycoder777-site.oss-cn-chengdu.aliyuncs.com/21707272915_.pic.jpg
+#   actions:
+#     - theme: brand
+#       text: 开始探索
+#       link: /docs/article/markdown-examples
+#     - theme: alt
+#       text: 项目
+#       link: /docs/article/markdown-examples
     # - theme: brand
     #   text: 书库
     #   link: /docs/article/markdown-examples
