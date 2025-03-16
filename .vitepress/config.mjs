008969ba3a97def6729e63e78a02acea7c54379c
atao <87337334@qq.com>
2025-03-16 15:35:51 +0800

    新增

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -105,7 +105,7 @@ export default defineConfig({
       { icon: 'github', link: 'https://github.com/binarycoder777' },
       { icon: 'twitter', link: 'https://x.com/binarycoder777' },
       { icon: 'discord', link: 'https://discord.gg/7k3fsuas' },
-      { icon: 'vercel', link: 'https://vercel.com/binarycoder777s-projects' }
+      // { icon: 'vercel', link: 'https://vercel.com/binarycoder777s-projects' }
     ],
 
     footer: {
