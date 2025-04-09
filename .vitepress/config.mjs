f3861f6c859b8c7e1d2e4a49e6fec950cd78feaa
atao <87337334@qq.com>
2025-04-09 20:00:24 +0800

    修改主页

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -86,18 +86,18 @@ export default defineConfig({
             text: "ChatGPT",
             link: "https://chat.openai.com/"
           },
-          {
-            text: "个人周刊",
-            link: "https://binarycoder777.com"
-          },
-          {
-            text:"个人工具",
-            link:"https://personal-k7of91wlx-binarycoder777s-projects.vercel.app/"
-          },
-          {
-            text: "LobChat",
-            link: "https://lobe-chat-eight-virid-32.vercel.app/chat"
-          },
+          // {
+          //   text: "个人周刊",
+          //   link: "https://binarycoder777.com"
+          // },
+          // {
+          //   text:"个人工具",
+          //   link:"https://personal-k7of91wlx-binarycoder777s-projects.vercel.app/"
+          // },
+          // {
+          //   text: "LobChat",
+          //   link: "https://lobe-chat-eight-virid-32.vercel.app/chat"
+          // },
           {
             text: "iFixit",
             link: "https://zh.ifixit.com/"
